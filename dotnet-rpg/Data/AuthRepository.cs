faeb2b0893f58b49be587a6104f0d90363084696
Zavodny Petr <petr.zavodny@sensearena.com>
2024-05-03 09:51:40 +0200

    up to lesson 51.

--- dotnet-rpg/Data/AuthRepository.cs
+++ dotnet-rpg/Data/AuthRepository.cs
@@ -1,16 +1,21 @@
-﻿using System.Security.Cryptography;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using dotnet_rpg.Models;
+using Microsoft.IdentityModel.Tokens;
 
 namespace dotnet_rpg.Data;
 
 public class AuthRepository : IAuthRepository
 {
     private readonly DataContext _context;
+    private readonly IConfiguration _configuration;
 
-    public AuthRepository(DataContext context)
+    public AuthRepository(DataContext context, IConfiguration configuration)
     {
         _context = context;
+        _configuration = configuration;
     }
 
     public async Task<ServiceResponse<int>> Register(User user, string password)
@@ -44,14 +49,19 @@ public class AuthRepository : IAuthRepository
         
         User? user = await _context.Users.FirstOrDefaultAsync(u => u.Username.ToLower().Equals(username.ToLower()));
 
-        if (user == null || !VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+        if (user == null)
         {
             response.Success = false;
-            response.Message = "Invalid username or password";
+            response.Message = "User not found";
+        }
+        else if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+        {
+            response.Success = false;
+            response.Message = "Wrong password";
         }
         else
         {
-            response.Data = user.Id.ToString();
+            response.Data = CreateToken(user);
             response.Message = "Login successful";
             response.Success = true;
         }
@@ -76,5 +86,38 @@ public class AuthRepository : IAuthRepository
         using HMACSHA512 hmac = new(passwordSalt);
         byte[] computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
         return computedHash.SequenceEqual(passwordHash);
+    }
+
+    private string CreateToken(User user)
+    {
+        List<Claim> claims = new()
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.Username),
+        };
+        
+        string? appSettingsToken = _configuration.GetSection("AppSettings:Token").Value;
+        
+        if (appSettingsToken is null)
+        {
+            throw new ArgumentNullException(nameof(appSettingsToken));
+        }
+        
+        SymmetricSecurityKey key = new(Encoding.UTF8.GetBytes(appSettingsToken));
+        
+        SigningCredentials credentials = new(key, SecurityAlgorithms.HmacSha512Signature);
+        
+        SecurityTokenDescriptor tokenDescriptor = new()
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(1),
+            SigningCredentials = credentials,
+        };
+        
+        JwtSecurityTokenHandler tokenHandler = new();
+        
+        SecurityToken securityToken = tokenHandler.CreateToken(tokenDescriptor);
+        
+        return tokenHandler.WriteToken(securityToken);
     }
 }
--- dotnet-rpg/Program.cs
+++ dotnet-rpg/Program.cs
@@ -3,6 +3,8 @@ global using Microsoft.EntityFrameworkCore;
 global using dotnet_rpg.Data;
 using dotnet_rpg.Services;
 using DotNetEnv;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +24,19 @@ builder.Services.AddScoped<ICharacterService, CharacterService>();
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value!)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+        };
+    });
+
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -32,6 +47,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
